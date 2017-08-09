1bdc3acfa3573b8b956c1d9d46c1b4499bca97a7
sundar <sundar@192.168.22.1>
2017-08-09 16:42:26 +0530

    Added the constant files

---
+++ src/constant/LIStrNumConstant.java
@@ -0,0 +1,5 @@
+package constant;
+
+public class LIStrNumConstant {
+	public static final String STR_ZERO = "0";
+}
--- src/service/ListedIPOService.java
+++ src/service/ListedIPOService.java
@@ -15,7 +15,9 @@ public class ListedIPOService {
 	
 	public boolean execute(){
 		
-		fetchIPOData();
+		for(int index = 0; index <= 25; index++) {
+			fetchIPOData();
+		}
 		
 		return true;
 	}
@@ -29,7 +31,7 @@ public class ListedIPOService {
 		StringBuffer sbIPOPageURL = null;
 		StringBuffer sbCompanyURL = null;
 		
-		String mmYearIPO;
+		String mmYearIPO = "";
 		String companyName;
 		String strCompanyURL;
 		String issuePrice ;
@@ -45,69 +47,80 @@ public class ListedIPOService {
 		
 		try{
 			sbIPOPageURL = new StringBuffer(LIStrLtrConstant.LI_IPO_URL).append(ipoPageNumber++);
+			
+			System.out.println(sbIPOPageURL);
+			
 			doc = Jsoup.connect(sbIPOPageURL.toString()).get();
 			contents = doc
 					.select("#ipoissueDiv_3 table tr td a[target]");
 			
+			mmYearIPO = "";
 			
-			for (Element content : contents) {
+			try{
 				
-				sbCompanyURL = new StringBuffer(LIStrLtrConstant.LI_companyUrl);
-				
-				mmYearIPO = content.parent().parent().parent()
-						.previousElementSibling().text().trim();// month &
-																// year of
-																// IPO Issue
-				companyName = content.text().trim();
-				strCompanyURL = (sbCompanyURL.append(content.attr("href"))).toString().trim();// Deriving IPO Link
-				issuePrice = content.parent().parent().parent()
-						.select("td:nth-of-type(2)").text();// Deriving IPO
-															// Issue Price
-				
-				companyDoc = Jsoup.connect(strCompanyURL).get();// Connecting to
-															// IPO Link
-				capValue = null;
-				capValue = companyDoc.select("#mktdet_2 .gD_12");// Market
-																			// Cap
-				bseValue = companyDoc.select(".PB10 .gry10");
-				info = bseValue.text();// BSE NSE ISIN INFO
-				bse = info.split("\\|")[0].split(":")[1].trim();
-				nse = info.split("\\|")[1].split(":")[1].trim();
-				isin = info.split("\\|")[2].split(":")[1].trim();
-				
-				if (bse.equals("") || bse.equals(" "))
-					bse = "0";
-				if (nse.equals("") || nse.equals(" "))
-					nse = "0";
-				if (isin.equals("") || isin.equals(" "))
-					isin = "0";
-				marketCap = capValue.first().text();
-				marketCap = marketCap.replaceAll("[,]", "");
-				if (mmYearIPO.matches("(.*)(\\-+)(\\d*)")) {
-					mmYearIPO = mmYearIPO.replaceAll("\\s", "");
-					date = mmYearIPO;
+				for (Element content : contents) {
+					
+					sbCompanyURL = new StringBuffer(LIStrLtrConstant.LI_companyUrl);
+					
+					if(mmYearIPO.isEmpty()) {
+						// month & year of IPO Issue
+						mmYearIPO = content.parent().parent().parent()
+							.previousElementSibling().text().trim();
+					}
+					companyName = content.text().trim();
+					strCompanyURL = (sbCompanyURL.append(content.attr("href"))).toString().trim();// Deriving IPO Link
+					issuePrice = content.parent().parent().parent()
+							.select("td:nth-of-type(2)").text();// Deriving IPO
+																// Issue Price
+					
+					companyDoc = Jsoup.connect(strCompanyURL).get();// Connecting to
+																// IPO Link
+					capValue = null;
+					capValue = companyDoc.select("#mktdet_2 .gD_12");// Market
+																				// Cap
+					bseValue = companyDoc.select(".PB10 .gry10");
+					info = bseValue.text();// BSE NSE ISIN INFO
+					bse = info.split("\\|")[0].split(":")[1].trim();
+					nse = info.split("\\|")[1].split(":")[1].trim();
+					isin = info.split("\\|")[2].split(":")[1].trim();
+					
+					if (bse.equals("") || bse.equals(" "))
+						bse = "0";
+					if (nse.equals("") || nse.equals(" "))
+						nse = "0";
+					if (isin.equals("") || isin.equals(" "))
+						isin = "0";
+					marketCap = capValue.first().text();
+					marketCap = marketCap.replaceAll("[,]", "");
+					
+					/*if (mmYearIPO.matches("(.*)(\\-+)(\\d*)")) {
+						mmYearIPO = mmYearIPO.replaceAll("\\s", "");
+						date = mmYearIPO;
+					}*/
+					
+					/*
+					if (companyName != null) {
+						System.out.println(date + " , " + companyName + " , "
+								+ companyUrl + " , " + issuePrice + " , "
+								+ marketCap + " , " + bse + " , " + nse + " , "
+								+ isin + " , " + currDate);
+						log.info("Info of " + companyName
+								+ " added in csv file...");
+						String bytes = date + " , " + companyName + " , "
+								+ companyUrl + " , " + issuePrice + " , "
+								+ marketCap + " , " + bse + " , " + nse + " , "
+								+ isin + " , " + currDate + "\n";
+						fos.write(bytes.getBytes());
+					}
+					
+					*/
 				}
+			} catch(Exception ex) {
 				
-				/*
-				if (companyName != null) {
-					System.out.println(date + " , " + companyName + " , "
-							+ companyUrl + " , " + issuePrice + " , "
-							+ marketCap + " , " + bse + " , " + nse + " , "
-							+ isin + " , " + currDate);
-					log.info("Info of " + companyName
-							+ " added in csv file...");
-					String bytes = date + " , " + companyName + " , "
-							+ companyUrl + " , " + issuePrice + " , "
-							+ marketCap + " , " + bse + " , " + nse + " , "
-							+ isin + " , " + currDate + "\n";
-					fos.write(bytes.getBytes());
-				}
-				
-				*/
 			}
 			
 		}catch(Exception ex) {
-			
+			System.out.println("stop");
 		}
 		
 		return true;
